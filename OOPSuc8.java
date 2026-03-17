8b5fe75d572cb6ccb7c7c8fb9e636df997632514
vyshnavi <vg4201@srmist.edu.in>
2026-03-17 23:48:23 +0530

    Add OOPSuc8.java

---
+++ OOPSuc8.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+public class OOPSuc8 {
+public static void main(String[] args) 
+{
+        HashMap<Character,String[]> charMap=createCharacterMap();
+        String message="OOPS";
+        displayBanner(message,charMap);
+}
+public static HashMap<Character,String[]> createCharacterMap() 
+{
+HashMap<Character,String[]> charMap=new HashMap<>();
+charMap.put('O',new String[]
+{
+"   ***   ",
+" **   ** ",
+"**     **",
+"**     **",
+"**     **",
+" **   ** ",
+"   ***   "
+});
+charMap.put('P', new String[]
+{
+"*****",
+"*    *",
+"*    *",
+"*    *",
+"*****",
+"*   ",
+"*   ",
+"*   "
+});
+charMap.put('S', new String[]
+{
+" *****",
+"*",
+"*",
+" *****",
+"      *",
+"       *",
+"  *****"
+});
+return charMap;
+}
+public static void displayBanner(String message,HashMap<Character,String[]>charMap) 
+{
+        int patternHeight = charMap.get('O').length;
+        for(int line=0;line<patternHeight;line++) 
+		{
+		    StringBuilder sb=new StringBuilder();
+            for(char ch:message.toUpperCase().toCharArray()) 
+			{
+                String[] pattern=charMap.get(ch);
+                if (pattern!=null) 
+				{
+                    sb.append(pattern[line]).append("  ");
+                }
+            }
+            System.out.println(sb.toString());
+        }
+    }
+}
