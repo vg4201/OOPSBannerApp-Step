303b6d6100d060e2eaa7db83bcd7fac0d15916ea
vyshnavi <vg4201@srmist.edu.in>
2026-03-17 21:07:24 +0530

    Add OOPSuc2.java

---
+++ OOPSuc2.java
@@ -0,0 +1,13 @@
+public class OOPSuc2 {
+    public static void main(String[] args) {
+        System.out.println("   ***     " + "   ***     " + " ******    " + "     ***** ");
+        System.out.println(" **   **   " + " **   **   " + "**    **   " + "  **       ");
+        System.out.println("**     **  " + "**     **  " + "**     **  " + "**         ");
+        System.out.println("**     **  " + "**     **  " + "**     **  " + "  *****    ");
+        System.out.println("**     **  " + "**     **  " + " ******    " + "      **   ");
+        System.out.println("**     **  " + "**     **  " + "**         " + "        ** ");
+        System.out.println("**     **  " + "**     **  " + "**         " + "       **  ");
+        System.out.println(" **   **   " + " **   **   " + "**         " + "       **  ");
+        System.out.println("   ***     " + "   ***     " + "**         " + "  *****    ");
+    }
+}
