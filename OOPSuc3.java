0ebe17548a24ee1a14d387f7ca8651cc40cbe4ae
vyshnavi <vg4201@srmist.edu.in>
2026-03-17 21:20:27 +0530

    add OOPSuc3.java

---
+++ OOPSuc3.java
@@ -0,0 +1,14 @@
+public class OOPSuc3 {
+    public static void main(String[] args) {
+        System.out.println(String.join("   ***     " , "   ***     " , " ******    " ,"     ***** "));
+        System.out.println(String.join(" **   **   " , " **   **   " , "**    **   " , "  **       "));
+        System.out.println(String.join("**     **  " , "**     **  " , "**     **  " , "**         "));
+        System.out.println(String.join("**     **  " , "**     **  " , "**     **  " , "  *****    "));
+        System.out.println(String.join("**     **  " , "**     **  " , " ******    " , "      **   "));
+        System.out.println(String.join("**     **  " , "**     **  " , "**         " , "        ** "));
+        System.out.println(String.join("**     **  " , "**     **  " , "**         " , "       **  "));
+        System.out.println(String.join(" **   **   " , " **   **   " , "**         " , "       **  "));
+        System.out.println(String.join("   ***     " , "   ***     " , "**         " , "  *****    "));
+        
+    }
+}
